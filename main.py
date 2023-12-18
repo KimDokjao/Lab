56697a392abb35749d4d2c793b901283af2b6ff1
KimDokjao <154261604+KimDokjao@users.noreply.github.com>
2023-12-19 00:36:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import statistics
 
 def import_file_to_postgres():
     engine=create_engine("postgresql://postgres:"+password+"@localhost:5432/postgres")
-    #tiny= 'nyc_yellow_tiny.csv'
     df=pd.read_csv(file)
     df.to_sql('trips', engine, if_exists='replace', index=False)
     print(df)
@@ -24,10 +23,10 @@ def median(a):
     return sum(sorted(a)[index - 1:index + 1]) / 2
 
 def postgress():
-    post1 = []
-    post2 = []
-    post3 = []
-    post4 = []
+    postgress_query1 = []
+    postgress_query2 = []
+    postgress_query3 = []
+    postgress_query4 = []
     try:
         conn= psycopg2.connect(
             user=user,
@@ -43,34 +42,34 @@ def postgress():
             cur.execute('SELECT "VendorID", count(*) FROM trips GROUP BY 1;')
             end_time = time.time()
             execution_time = end_time - start_time
-            post1.append(execution_time)
+            postgress_query1.append(execution_time)
             start_time = time.time()
             cur.execute("SELECT passenger_count, avg(total_amount) FROM trips GROUP BY 1;")
             cur.fetchall()
             end_time = time.time()
             execution_time = end_time - start_time
-            post2.append(execution_time)
+            postgress_query2.append(execution_time)
             start_time = time.time()
             cur.execute("SELECT passenger_count, extract(year from tpep_pickup_datetime), count(*) FROM trips GROUP BY 1, 2;")
             cur.fetchall()
             end_time = time.time()
             execution_time = end_time - start_time
-            post3.append(execution_time)
+            postgress_query3.append(execution_time)
             start_time = time.time()
             cur.execute( "SELECT passenger_count, extract(year from tpep_pickup_datetime), round(trip_distance), count(*) FROM trips GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;")
             cur.fetchall()
             end_time = time.time()
             execution_time = end_time - start_time
-            post4.append(execution_time)
+            postgress_query4.append(execution_time)
 
         cur.close()
         conn.close()
 
         psycopg2_time = []
-        psycopg2_time.append(median(post1))
-        psycopg2_time.append(median(post2))
-        psycopg2_time.append(median(post3))
-        psycopg2_time.append(median(post4))
+        psycopg2_time.append(median(postgress_query1))
+        psycopg2_time.append(median(postgress_query2))
+        psycopg2_time.append(median(postgress_query3))
+        psycopg2_time.append(median(postgress_query4))
 
 
         print("Execution time for SQL 1 in PostgreSQL: ", psycopg2_time[0], " seconds ")
@@ -81,14 +80,13 @@ def postgress():
     except psycopg2.Error as error:
         print("Error: ", error)
 
-lite1 = []
-lite2 = []
-lite3 = []
-lite4 = []
+sqlite_query1 = []
+sqlite_query2 = []
+sqlite_query3 = []
+sqlite_query4 = []
 
 def sqlite():
     engine = create_engine("sqlite:///database.db")
-        # tiny = 'nyc_yellow_tiny.csv'
     df = pd.read_csv(file)
     df.to_sql('trips', engine, if_exists='replace', index=False)
     print(df)
@@ -101,34 +99,34 @@ def sqlite():
         cur.fetchall()
         end_time = time.time()
         execution_time = end_time - start_time
-        lite1.append(execution_time)
+        sqlite_query1.append(execution_time)
         start_time = time.time()
         cur.execute("SELECT passenger_count, avg(total_amount) FROM trips GROUP BY 1;")
         cur.fetchall()
         end_time = time.time()
         execution_time = end_time - start_time
-        lite2.append(execution_time)
+        sqlite_query2.append(execution_time)
         start_time = time.time()
         cur.execute("SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), count(*) FROM trips GROUP BY 1, 2;")
         cur.fetchall()
         end_time = time.time()
         execution_time = end_time - start_time
-        lite3.append(execution_time)
+        sqlite_query3.append(execution_time)
         start_time = time.time()
         cur.execute("SELECT passenger_count, strftime('%Y', tpep_pickup_datetime), round(trip_distance), count(*) FROM trips GROUP BY 1, 2, 3 ORDER BY 2, 4 DESC;")
         cur.fetchall()
         end_time = time.time()
         execution_time = end_time - start_time
-        lite4.append(execution_time)
+        sqlite_query4.append(execution_time)
 
     cur.close()
     conn.close()
 
     sqllite_time = []
-    sqllite_time.append(median(lite1))
-    sqllite_time.append(median(lite2))
-    sqllite_time.append(median(lite3))
-    sqllite_time.append(median(lite4))
+    sqllite_time.append(median(sqlite_query1))
+    sqllite_time.append(median(sqlite_query2))
+    sqllite_time.append(median(sqlite_query3))
+    sqllite_time.append(median(sqlite_query4))
 
     print("Execution time for SQL 1 in SQLite: ", sqllite_time[0] ," seconds ")
     print("Execution time for SQL 2 in SQLite: ", sqllite_time[1], " seconds ")
@@ -136,10 +134,10 @@ def sqlite():
     print("Execution time for SQL 4 in SQLite: ", sqllite_time[3], " seconds ")
 
 
-pan1 = []
-pan2 = []
-pan3 = []
-pan4 = []
+pandas_query1 = []
+pandas_query2 = []
+pandas_query3 = []
+pandas_query4 = []
 
 def median(a):
     n = len(a)
@@ -151,7 +149,7 @@ def median(a):
 
 
 def pandas():
-    trips = pd.read_csv("nyc_yellow_tiny.csv")
+    trips = pd.read_csv(file)
     trips['tpep_pickup_datetime'] = pd.to_datetime(trips['tpep_pickup_datetime'])
 
 
@@ -160,28 +158,28 @@ def pandas():
         result1 = trips.groupby("VendorID").size()
         end_time = time.time()
         execution_time = end_time - start_time
-        pan1.append(execution_time)
+        pandas_query1.append(execution_time)
         start_time = time.time()
         result2 = trips.groupby('passenger_count')['total_amount'].mean()
         end_time = time.time()
         execution_time = end_time - start_time
-        pan2.append(execution_time)
+        pandas_query2.append(execution_time)
         start_time = time.time()
         result3 = trips.groupby(['passenger_count', trips['tpep_pickup_datetime'].dt.year]).size()
         end_time = time.time()
         execution_time = end_time - start_time
-        pan3.append(execution_time)
+        pandas_query3.append(execution_time)
         result4 = trips.groupby(['passenger_count', trips['tpep_pickup_datetime'].dt.year, trips['trip_distance'].round()]).size().reset_index(name='count').sort_values(['tpep_pickup_datetime', 'count'], ascending=[True, False])
         end_time = time.time()
         execution_time = end_time - start_time
-        pan4.append(execution_time)
+        pandas_query4.append(execution_time)
         start_time = time.time()
 
     pandas_time = []
-    pandas_time.append(median(pan1))
-    pandas_time.append(median(pan2))
-    pandas_time.append(median(pan3))
-    pandas_time.append(median(pan4))
+    pandas_time.append(median(pandas_query1))
+    pandas_time.append(median(pandas_query2))
+    pandas_time.append(median(pandas_query3))
+    pandas_time.append(median(pandas_query4))
 
     print("Execution time for SQL 1 in Pandas:",     pandas_time[0], " seconds ")
     print("Execution time for SQL 2 in Pandas: ",    pandas_time[1], " seconds ")
@@ -191,7 +189,6 @@ def pandas():
 
 
 def duck_db():
-
     conn = duckdb.connect(database=':memory:', read_only=False)
     df = pd.read_csv(file)
     conn.register('trips', df)
